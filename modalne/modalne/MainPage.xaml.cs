53a8e3b16e6d53b16c2b7647a52c685c698282b6
4PTP_2 <4PTP_2@DESKTOP-HMD73UL>
2023-09-06 11:43:34 +0200

    Dodaj pliki projektów.

---
+++ modalne/modalne/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace modalne
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new ModalPage());
+        }
+
+        private async void alert(object sender, EventArgs e)
+        {
+            await DisplayAlert("Alert", "You have been alerted", "Ok");
+        }
+
+        private async void sheet(object sender, EventArgs e)
+        {
+            string actionSendTo = await DisplayActionSheet("AcionSheet: Send to?", "Cancel", null, "Email", "Twitter", "Facebook");
+        }
+
+        private async void prompt(object sender, EventArgs e)
+        {
+            string resultQuestion = await DisplayPromptAsync("Question 2", "What's 5 + 5?", initialValue: "10", maxLength: 2, keyboard: Keyboard.Numeric);
+        }
+    }
+}
---
+++ modalne/modalne/ModalPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace modalne
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ModalPage : ContentPage
+    {
+        public ModalPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void backbtn(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+    }
+}
